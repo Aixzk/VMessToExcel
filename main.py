4cf7b560882826b868c7fc7f25297d01b9252482
PandaTail <772480028@qq.com>
2021-11-01 20:28:13 +0800

    补充说明

--- main.py
+++ main.py
@@ -9,20 +9,24 @@ if __name__ == '__main__':
     # 然后就可以把 sub 订阅文件上传至网络中，并用网址订阅
     # 注：暂未开发异常处理，以及更多应用场景和方法自行开发
 
+    # 订阅链接列表
     subUrls = [
-        'http://xxx.xxx/sub1',
-        'http://xxx.xxx/sub2',
-        'http://xxx.xxx/sub3'
+        'http://sub1',
+        'http://sub2',
+        'http://sub3'
     ]
 
+    # 加载订阅链接内容
     nodesDictList = []
     for subUrl in subUrls:
-        print(f'获取：{subUrl}')
+        print(f'正在获取：{subUrl}')
         nodesDictList += LoadSubUrl(subUrl)
+
+    # 保存到运行目录下的 nodes.xls
     SaveToExcel(nodesDictList, './nodes.xls')
 
+    # 将运行目录下的 nodes.xls 转换为 sub 订阅文件
     input('回车后将 nodes.xls 转换为 sub 订阅文件')
-
     newNodesDictList = ReadExcel('./nodes.xls')
     SaveToSubFile(newNodesDictList, './sub')
 
